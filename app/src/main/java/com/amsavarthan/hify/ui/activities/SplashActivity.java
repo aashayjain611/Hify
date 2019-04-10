3931cbef280baca35d844d6d58989e2b2c983577
Anukrit Jain <anukrit.jain@gmail.com>
2019-04-11 02:45:18 +0530

    Splash added

---
+++ app/src/main/java/com/amsavarthan/hify/ui/activities/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.amsavarthan.hify.ui.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.amsavarthan.hify.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+    }
+
+}
